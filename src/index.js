6e3747a57b9e8a426b3f1b3674454163a47ae22a
Chathu Abeywickrama <chathu.git93@gmail.com>
2019-12-27 08:05:32 +0530

    notistack used

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from 'notistack';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <SnackbarProvider>
+            <App />
+        </SnackbarProvider>
     </BrowserRouter>
 
     , document.getElementById('root')
