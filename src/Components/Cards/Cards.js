2dc01c8f1cc8d5a965881305e60bf869c4fe5a02
Chathu Abeywickrama <chathu.git93@gmail.com>
2019-12-27 11:45:40 +0530

    cards taken as seperate component

---
+++ src/Components/Cards/Cards.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+import AppNavBar from "../../Components/AppNavBar/AppNavBar";
+import Copyright from "../../Components/Copyright/Copyright";
+
+const useStyles = makeStyles(theme => ({
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
+
+
+
+export default function Cards(props) {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+
+            <CssBaseline />
+
+            <main>
+
+                <Container className={classes.cardGrid} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+
+                        {props.cardData.map(card => (
+                            <Grid item key={props.cardData} xs={12} sm={6} md={4}>
+
+                                <Card className={classes.card}>
+
+                                    <CardMedia
+                                        className={classes.cardMedia}
+                                        image="https://source.unsplash.com/random"
+                                        title="Image title"
+                                    />
+
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom variant="h5" component="h2">{card.heading}</Typography>
+
+                                        <Typography>{card.content}</Typography>
+
+                                    </CardContent>
+
+                                    <CardActions>
+
+                                        <Button size="small" color="primary">
+                                            View
+                                        </Button>
+
+                                        <Button size="small" color="primary">
+                                            Edit
+                                         </Button>
+
+                                    </CardActions>
+
+                                </Card>
+
+                            </Grid>
+                        ))}
+                    </Grid>
+
+                </Container>
+
+            </main>
+
+        </React.Fragment>
+    );
+}
