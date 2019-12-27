1289f72c5b530ef0954427d12facc5705a8e88c6
Chathu Abeywickrama <chathu.git93@gmail.com>
2019-12-27 06:55:31 +0530

    Create Main.js

---
+++ src/Views/Main/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+// Routes
+import Landing from '../Landing/Landing';
+import Login from '../../Components/Login/Login';
+import Register from "../../Components/Register/Register"
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    componentDidMount() { }
+
+    render() {
+        return (
+            <main>
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                </Switch>
+            </main>
+        );
+    }
+}
+
+export default Main;
